63985cc6eb347bacefd349b9e29e4d24af97d051
LilB333 <renal_turaev@mail.ru>
2025-09-25 11:11:12 +0300

    maintaining

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -14,8 +14,8 @@ def set_up():
         # Настройки Chrome
         chrome_options = Options()
 
-        # Сначала устанавливаем headless режим
-        chrome_options.add_argument('--headless=new')
+        # Сначала устанавливаем headless режим (для CI/CD)
+        #chrome_options.add_argument('--headless=new')
 
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
@@ -25,28 +25,34 @@ def set_up():
         chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
         chrome_options.add_argument("--disable-logging")
 
+        # Для избежания таймаутов
+        chrome_options.add_argument("--disable-features=VizDisplayCompositor")
+        chrome_options.add_argument("--disable-accelerated-2d-canvas")
+        chrome_options.add_argument("--disable-background-timer-throttling")
+
         # Путь к драйверу
         service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=chrome_options)
 
-        driver.execute_cdp_cmd(
-            "Emulation.setDeviceMetricsOverride",
-            {
-                "width": 1920,
-                "height": 1080,
-                "deviceScaleFactor": 1,
-                "mobile": False,
-                "screenWidth": 1920,
-                "screenHeight": 1080,
-                "viewport": {
-                    "x": 0,
-                    "y": 0,
-                    "width": 1920,
-                    "height": 1080,
-                    "scale": 1
-                }
-            }
-        )
+
+        # driver.execute_cdp_cmd(
+        #     "Emulation.setDeviceMetricsOverride",
+        #     {
+        #         "width": 1920,
+        #         "height": 1080,
+        #         "deviceScaleFactor": 1,
+        #         "mobile": False,
+        #         "screenWidth": 1920,
+        #         "screenHeight": 1080,
+        #         "viewport": {
+        #             "x": 0,
+        #             "y": 0,
+        #             "width": 1920,
+        #             "height": 1080,
+        #             "scale": 1
+        #         }
+        #     }
+        # )
 
         # Открытие сайта
         driver.get(url)
